8b4e5a15fd13ee75c8af570c5059cfa1a9f2a455
mh-rabbi <mhr221official@gmail.com>
2025-06-05 01:20:52 +0600

    Added: NavigationDrawer(menu->nav_menu.xml,layout->nav_header,Some drawable files), Fragments added

--- app/src/main/java/com/vrgc/eguidance/Activity/HomeActivity.java
+++ app/src/main/java/com/vrgc/eguidance/Activity/HomeActivity.java
@@ -1,23 +1,90 @@
 package com.vrgc.eguidance.Activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.navigation.NavigationView;
+import com.vrgc.eguidance.Fragments.HomeFragment;
+import com.vrgc.eguidance.Fragments.ProfileFragment;
 import com.vrgc.eguidance.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
 
+        Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
+        setSupportActionBar(toolbar);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
+                R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+        replaceFragment(new HomeFragment());
+    }
+
+    private  void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.nav_home) {
+            replaceFragment(new HomeFragment());
+        }else if (itemId == R.id.nav_profile) {
+            replaceFragment(new ProfileFragment());
+//        }
+//        else if (itemId == R.id.nav_settings) {
+//            replaceFragment(new PofileSettingsFragment());
+//        } else if (itemId == R.id.nav_share) {
+//            replaceFragment(new ShareFragment());
+//        } else if (itemId == R.id.nav_about) {
+//            replaceFragment(new AboutUsFragment());
+        } else if (itemId == R.id.nav_logout) {
+            Toast.makeText(this, "Logout Successfully!", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/vrgc/eguidance/Fragments/ProfileFragment.java
@@ -0,0 +1,6 @@
+package com.vrgc.eguidance.Fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class ProfileFragment extends Fragment {
+}
